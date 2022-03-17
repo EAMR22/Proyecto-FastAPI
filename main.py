fef98f9861ad4e38c6d564b79ee2556df531b0ba
elvishn <elvis1042.rodriguez@gmail.com>
2022-03-17 11:28:27 -0600

    files

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pydantic import EmailStr
 # FastApi
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form, Header, Cookie
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 
 app = FastAPI()
 
@@ -185,3 +185,18 @@ def contact(
     ads: Optional[str] = Cookie(default=None)
 ):
     return user_agent
+
+# Files:
+
+@app.post(
+    path="/post-image"
+)
+def post_image(
+    image: UploadFile = File(...)
+):
+    return {
+        "Filename": image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round(len(image.file.read())/1024, ndigits=2)
+    }
+
